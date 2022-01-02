ff6163e69b343ff5ab706f41b60942bffcd5a209
Sidouuuux <mirsidahmed@hotmail.fr>
2022-01-02 16:25:26 +0100

    deploy contract

--- deploy.py
+++ deploy.py
@@ -1,5 +1,12 @@
+from eth_account import account
 from solcx import compile_standard, install_solc
 import json
+from web3 import Web3
+import web3
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 with open("./SimpleStorage.sol", "r") as file:
     simple_storage_file = file.read()
@@ -18,7 +25,6 @@ compiled_sol = compile_standard(
     },
     solc_version="0.6.0",
 )
-print(compiled_sol)
 
 with open("compiled_code.json", "w") as file:
     json.dump(compiled_sol, file)
@@ -31,3 +37,28 @@ bytecode = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["evm"
 abi = json.loads(
     compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["metadata"]
 )["output"]["abi"]
+
+w3 = Web3(Web3.HTTPProvider("http://127.0.0.1:7545"))
+chain_id = 1337
+account_address = "0x683fa4c62E45d8937BC2cc305784553859619172"
+account_key = os.getenv("ACCOUNT_KEY")
+SimpleStorage = w3.eth.contract(abi=abi, bytecode=bytecode)
+
+nonce = w3.eth.getTransactionCount(account_address)
+print("nonce : " + str(nonce))
+
+transaction = SimpleStorage.constructor().buildTransaction(
+    {
+        "chainId": chain_id,
+        "gasPrice": w3.eth.gas_price,
+        "from": account_address,
+        "nonce": nonce,
+    }
+)
+
+signed_txn = w3.eth.account.sign_transaction(transaction, private_key=account_key)
+print(signed_txn)
+
+tx_hash = w3.eth.send_raw_transaction(signed_txn.rawTransaction)
+tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
+print(tx_receipt)
