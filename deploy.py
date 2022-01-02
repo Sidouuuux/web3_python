b590456592489069f089f98799950e82efa89245
Sidouuuux <mirsidahmed@hotmail.fr>
2022-01-02 15:56:56 +0100

    contract compilation

---
+++ deploy.py
@@ -0,0 +1,33 @@
+from solcx import compile_standard, install_solc
+import json
+
+with open("./SimpleStorage.sol", "r") as file:
+    simple_storage_file = file.read()
+install_solc("0.6.0")
+compiled_sol = compile_standard(
+    {
+        "language": "Solidity",
+        "sources": {"SimpleStorage.sol": {"content": simple_storage_file}},
+        "settings": {
+            "outputSelection": {
+                "*": {
+                    "*": ["abi", "metadata", "evm.bytecode", "evm.bytecode.sourceMap"]
+                }
+            }
+        },
+    },
+    solc_version="0.6.0",
+)
+print(compiled_sol)
+
+with open("compiled_code.json", "w") as file:
+    json.dump(compiled_sol, file)
+
+
+bytecode = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["evm"][
+    "bytecode"
+]["object"]
+
+abi = json.loads(
+    compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["metadata"]
+)["output"]["abi"]
